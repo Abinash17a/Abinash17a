802d6768aed3d4e77c15e2c0b37a8b0852e4bfb7
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2022-09-23 00:44:58 +0530

    Updates

---
+++ precache-manifest.ff76155979216f643df212dd69131585.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "965547f593ca7c50be96",
+    "url": "/Abinash17a/static/css/main.0ebe6d46.chunk.css"
+  },
+  {
+    "revision": "965547f593ca7c50be96",
+    "url": "/Abinash17a/static/js/main.965547f5.chunk.js"
+  },
+  {
+    "revision": "4fee8dca46c6d262eedf",
+    "url": "/Abinash17a/static/js/1.4fee8dca.chunk.js"
+  },
+  {
+    "revision": "9240ddb00188849e3366",
+    "url": "/Abinash17a/static/js/runtime~main.9240ddb0.js"
+  },
+  {
+    "revision": "6f820574e0c1d3087097684bba9e1def",
+    "url": "/Abinash17a/static/media/webdev.6f820574.jpg"
+  },
+  {
+    "revision": "887afe1b320569750a2eac301292f8c3",
+    "url": "/Abinash17a/static/media/webdevc.887afe1b.jpg"
+  },
+  {
+    "revision": "31fa29769a856770d5f927106b6b5026",
+    "url": "/Abinash17a/static/media/webdeva.31fa2976.jpg"
+  },
+  {
+    "revision": "d0c520fb4554749e57859e8eb914b4c0",
+    "url": "/Abinash17a/static/media/newabout.d0c520fb.jpg"
+  },
+  {
+    "revision": "f1b40ff9617a4dcf9bfc115ef6f274b6",
+    "url": "/Abinash17a/static/media/webdev1.f1b40ff9.jpg"
+  },
+  {
+    "revision": "f7584b2a1e08f5bdd66555640f037dd0",
+    "url": "/Abinash17a/static/media/TExter.f7584b2a.png"
+  },
+  {
+    "revision": "fdc10367acdaacd7947c5a27bdb7423c",
+    "url": "/Abinash17a/static/media/drum.fdc10367.png"
+  },
+  {
+    "revision": "bd6a8ce064085ea4823c066e1f02ad19",
+    "url": "/Abinash17a/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9de2db523ed64dab08cb539206ea34d5.js"
+  "/Abinash17a/precache-manifest.ff76155979216f643df212dd69131585.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Abinash17a/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
